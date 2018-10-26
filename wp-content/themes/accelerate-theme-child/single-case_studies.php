9f2cafadd00229413a8de9e0b8448ef8554f590c
Victoria <victoria@phyllisandeugenia.com>
2018-10-26 16:14:26 -0400

    single case studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -15,5 +15,15 @@
 get_header(); ?>
 
 
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+
+	</div><!-- #primary -->
+
 
 <?php get_footer(); ?>
