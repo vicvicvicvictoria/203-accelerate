f44a5201bd5d86a6f5d65bc35fb08253cf16a5fa
Victoria <victoria@phyllisandeugenia.com>
2018-10-26 20:11:39 -0400

    custom fields

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -17,13 +17,29 @@ get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$link = get_field('site_link'); ?>
+
+				<article class="case-study">
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h4><?php echo $services; ?></h4>
+
+						<?php the_content(); ?>
+
+						<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site ›</a></p>
+					</aside>
+				</article>
+			<?php endwhile; ?>
 		</div><!-- .main-content -->
 
 
 	</div><!-- #primary -->
-
+	<div class="case-study-images">
+		<?php if($image_1) {
+			echo wp_get_attachment_image($image, $size);
+		 } ?>
 
 <?php get_footer(); ?>
