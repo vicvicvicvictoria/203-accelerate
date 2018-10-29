95b74682ebe4a0bb1c19e2bb1cf4ffdb9249bbf6
Victoria <victoria@phyllisandeugenia.com>
2018-10-29 19:55:57 -0400

    case studies

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,7 +23,28 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
 
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+			 <?php while ( have_posts() ) : the_post(); ?>
+				 $image_1 = get_field("image_1");
+				 $size = "medium";
+				 ?>
+				 <li class="individual-featured-work">
+				   <figure>
+					   <?php echo wp_get_attachment_image($image_1, $size); ?>
+				   </figure>
+			 </li>
+				 <h3><a href="<?php the_permalink(); ?>"></a></h3>
+				 <?php the_excerpt (); ?>
+			 <?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+	</ul>
+	</div>
+</section>
 
 			<section class="recent-posts">
 				<div class="site-content">
